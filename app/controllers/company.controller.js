6fce32c9821f212c290d6220cedce93633459081
Harjinder <harjinder0@gmail.com>
2023-01-09 21:06:37 +0530

    added company apis

---
+++ app/controllers/company.controller.js
@@ -0,0 +1,61 @@
+const db = require("../models");
+const Company = db.company;
+
+
+
+// Create and Save a new company
+exports.add = (req, res) => {
+    const {name, address, logo, id_number} = req.body;
+
+    // Validate request
+    if (!name && !address && !id_number) {
+        res.status(400).send({ 
+            status: false, 
+            message: "Fields can not be empty!" 
+        });
+        return;
+    }
+
+    // check if company already exist
+    Company.findOne({ id_number:  id_number })
+    .then(function(result){
+        if (result) {
+            res.status(400).send({ 
+                status: false, 
+                message: "Company already registered.",
+                data: result
+            });
+            return;
+        } 
+    });
+    
+    // Create a User
+    const company = new Company({
+        name: name,
+        address: address,
+        logo: logo,
+        id_number: id_number
+    });
+
+       
+        
+    // Save User in the database
+    user
+    .save(user)
+    .then(data => {
+        res.send({
+        status: true,
+        message: "Signup successful",
+        data: data
+        });
+    })
+    .catch(err => {
+        res.status(500).send({
+        status: false,
+        message:
+            err.message || "Some error occurred while creating the User."
+        });
+    });
+};
+
+      
