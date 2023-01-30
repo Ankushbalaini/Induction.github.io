196116d239638818d4943913cd4448e070d5dfc6
Harjinder <harjinder0@gmail.com>
2023-01-30 15:54:29 +0530

    Start test page design

---
+++ frontend/src/jsx/components/Modals/test/TestQuestions.js
@@ -0,0 +1,17 @@
+import React from "react";
+import BackgroundModal from "../../Modals/test/BackgroundModal";
+
+const TestQuestions = (props) => {
+  return (
+    <BackgroundModal>
+      <div className="background-modal">
+        Display Questions here
+      </div>
+      
+    </BackgroundModal>
+  );
+};
+
+export default TestQuestions;
+
+
