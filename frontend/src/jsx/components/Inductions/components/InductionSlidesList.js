cfa986e396d5e6cecc4a90303e987f6263dcfa47
PrabhleenKaurGujral <prabhleen.gujral@bjshomedelivery.com>
2023-02-27 12:31:08 +0530

    graph dummy

--- frontend/src/jsx/components/Inductions/components/InductionSlidesList.js
+++ frontend/src/jsx/components/Inductions/components/InductionSlidesList.js
@@ -30,16 +30,16 @@ function InductionSlidesList({ setCurrentSlideContent, ...props }) {
   // const [steps, setSteps] = useState([]);
   const [active, setActive] = useState(0);
 
-  // const [keyarr, setKeyarr]=useState('')
+  const [keyarr, setKeyarr]=useState('')
 
-  // const ButtonPress = (event) =>{
-  //   if(event.keyCode == 38){
+  // const ButtonPress = (e) =>{
+  //   if(e.keyCode == 38){
   //       alert('Adding....');
   //   }else(
   //     alert("nothing")
   //   )
   // }
-
+  
 
   const handleClick = (slideData, i) => {
     const totalSlides = props.slides.length;
@@ -244,11 +244,11 @@ function InductionSlidesList({ setCurrentSlideContent, ...props }) {
             </div>
           ) : null}
         </Accordion>
-        {/* <p>Key pressed is: {keyarr}</p>
+      
   
         
  
-  <input type="text" onKeyPress={(e) => handler(e)} /> */}
+  {/* <input type="text" onKeyPress={(e) => ButtonPress(e)} />  */}
         
       </div>
     </div>
