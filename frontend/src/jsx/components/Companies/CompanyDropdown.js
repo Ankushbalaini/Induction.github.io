972cc7699a34d52d125fc1bfe629383b6cd614af
vbvsingh905 <vaibhav.singh@bjshomedelivery.com>
2023-02-27 15:59:15 +0530

    added company api validations

--- frontend/src/jsx/components/Companies/CompanyDropdown.js
+++ frontend/src/jsx/components/Companies/CompanyDropdown.js
@@ -1,12 +1,14 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 
 // api call
-async function getCompanies() {
+async function getCompanies(token) {
     return fetch("http://localhost:8081/api/company/companyDropdownList", {
         method: "GET",
         headers: {
         "Content-Type": "application/json",
+        "x-access-token" : token
         },
         body: JSON.stringify(),
     }).then((data) => data.json());
@@ -14,13 +16,14 @@ async function getCompanies() {
 
    
 const CompanyDropdown = (props) => {
+    const token = useSelector((state) => state.auth.auth.token);
     const [selected, setSelected] = useState(props.prevSelected);
     const [loading, setLoading] = useState(true);
     const [option, setOption] = useState();
 
     const callApi = async () =>{
-        const response = await getCompanies();
-        if ("status" in response && response.status == true) {
+        const response = await getCompanies(token);
+        if ("status" in response && response.status == true) { 
             const rows = response.data.map((row, index) => (
                 <option value={row._id}>{ row?.company?.name ?? row._id}</option>
             ));
