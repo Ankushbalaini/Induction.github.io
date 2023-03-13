ab9062aae6f2dd1ed0d641e74c850d45b7a3a823
Harjinder <harjinder0@gmail.com>
2023-03-13 17:21:09 +0530

    forgot password link

--- frontend/src/jsx/constants.js
+++ frontend/src/jsx/constants.js
@@ -7,18 +7,18 @@ export const USER_ROLES = {
     USER: "user",
 };
 
-// export const API_ROOT_URL = process.env.API_ROOT_URL || "http://176.58.103.115:5000/api";
-export const API_ROOT_URL = "http://localhost:8081/api";
+export const API_ROOT_URL = process.env.API_ROOT_URL || "http://176.58.103.115:5000/api";
+// export const API_ROOT_URL = "http://localhost:8081/api";
 
-//export const UPLOADS_URL = `http://bjsinduction.com/api/`;
-export const UPLOADS_URL = `images`;
+export const UPLOADS_URL = `http://bjsinduction.com/api/`;
+// export const UPLOADS_URL = `images`;
 
 
-//export const PROFILE_ASSETS_URL = `http://bjsinduction.com/api/profile`;
-//export const INDUCTION_ASSETS_URL = `http://bjsinduction.com/api/inductions`;
+export const PROFILE_ASSETS_URL = `http://bjsinduction.com/api/profile`;
+export const INDUCTION_ASSETS_URL = `http://bjsinduction.com/api/inductions`;
 
-export const PROFILE_ASSETS_URL = `http://localhost/api.bjsinduction.com/images/profile`;
-export const INDUCTION_ASSETS_URL = `http://localhost/api.bjsinduction.com/images/inductions`;
+//export const PROFILE_ASSETS_URL = `http://localhost/api.bjsinduction.com/images/profile`;
+//export const INDUCTION_ASSETS_URL = `http://localhost/api.bjsinduction.com/images/inductions`;
 
 
 
